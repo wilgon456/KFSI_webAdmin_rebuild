123cdb9a21e7948aa605114f2f7aed5755fd6d6b
wilgon456 <juncatz1945@gmail.com>
2023-02-16 18:42:21 +0900

    230216_index,pwdsearch,pwdchange done

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 function showPopupMove() {
     window.open("https://naver.com","콘텐츠 이동","width=400, height=300, top=10, left=10")
 };
+
+
+
+const Btn = document.querySelector('.pwdBtn');
+
+function addBtnTxt() {
+    Btn.addEventListener('click', addTxt);
+}
+
+function addTxt(e) {
+    Btn.innerHTML = "<input type='text'> <input value='✅전송됨'>";
+}
+
+addBtnTxt();
